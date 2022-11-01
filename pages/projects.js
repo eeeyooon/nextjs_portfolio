2a0592552d098bcd404bba89b41cce3276e071c5
jiyoon <vywns9978@gmail.com>
2022-11-01 12:23:45 +0900

    tailwindCSS 적용

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,9 @@
+import Layout from "../components/layout";
 
 export default function Projects() {
     return (
-        <>
+        <Layout>
             <h1>프로젝트</h1>
-        </>
+        </Layout>
     );
 }
