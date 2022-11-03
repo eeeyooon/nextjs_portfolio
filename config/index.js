cb1b856446575659b772912cf242cd70d7fb55d0
jiyoon <vywns9978@gmail.com>
2022-11-04 00:33:35 +0900

    Notion DB에서 Next.js로 데이터 가져오기

---
+++ config/index.js
@@ -0,0 +1,4 @@
+// 설정들에 대한 파일들을 가져올 것.
+
+export const DATABASE_ID = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
+export const TOKEN = process.env.NEXT_PUBLIC_NOTION_TOKEN;
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout";
 import Head from "next/head";
+import { TOKEN, DATABASE_ID } from "../config";
 
 export default function Projects() {
   return (
@@ -12,4 +13,32 @@ export default function Projects() {
       <h1>프로젝트</h1>
     </Layout>
   );
+}
+
+// 처음 빌드 될 때 (빌드타임) 호출됨. ->데이터를 가져온 다음에 화면에 렌더링됨.
+export async function getStaticProps() {
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+
+  //응답값을 json으로 만들어줌.
+  const result = await res.json();
+
+  console.log(result);
+
+  return {
+    props: {},
+  };
 }
